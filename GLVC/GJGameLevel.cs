eda6e0db0fbd32c31992a4722627c039d269266e
Lucas Tozo <lucastozo7@gmail.com>
2023-12-26 10:10:03 -0400

    Revert "initial release"
    This reverts commit 2767a468e68cf36d2cd970786d890e245dd4ad04.

--- GLVC/GJGameLevel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GLVC
-{
-    public class GJGameLevel
-    {
-        public string? LevelString { get; set; }
-        public int AudioTrack { get; set; }
-        public List<string> Objects { get; set; } = new List<string>();
-    }
-}
